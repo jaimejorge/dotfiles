9ecf170b2bb37ec91b5b9bc6f2a4228244d58ec9
Cameron Malek <cam@cameronmalek.com>
2015-03-25 13:58:27 -0500

    moved keys.zsh into aliases.zsh

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -8,3 +8,9 @@ then
   alias ll="gls -l --color"
   alias la='gls -A --color'
 fi
+alias update='brew update;brew upgrade;gem update --system;gem update;~/.vim-anywhere/update'
+alias iflicks='mv ~/Movies/iFlicks\ 2/* ~/Music/iTunes/iTunes\ Media/Automatically\ Add\ to\ iTunes.localized/'
+alias la='ls -lha'
+
+# Pipe my public key to my clipboard.
+alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
