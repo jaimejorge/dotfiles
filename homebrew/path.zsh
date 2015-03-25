6f478e69c88824b58817a2fc6d242b3682f9124a
Cameron Malek <cam@cameronmalek.com>
2015-03-25 14:06:01 -0500

    add coreutils to path and manpath

---
+++ homebrew/path.zsh
@@ -0,0 +1,7 @@
+# Check for Homebrew
+if test $(which brew)
+then
+  export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+  export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+fi
+
