b53e9340cf28c93b1241455a1fd9aeb97277a6bd
Cameron Malek <cam@cameronmalek.com>
2015-09-24 15:42:19 -0500

    emptytrash clears download history from quarantine

--- osx/aliases.zsh
+++ osx/aliases.zsh
@@ -20,7 +20,8 @@ alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && k
 
 # Empty the Trash on all mounted volumes and the main HDD
 # Also, clear Apple’s System Logs to improve shell startup speed
-alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes/*; sudo rm -rfv ~/.Trash/*; sudo rm -rfv /private/var/log/asl/*.asl"
+# Finally, clear download history from quarantine. https://mths.be/bum
+alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes/*; sudo rm -rfv ~/.Trash/*; sudo rm -rfv /private/var/log/asl/*.asl;sqlite3 ~/Library/Preferences/com.apple.LaunchServices.QuarantineEventsV* 'delete from LSQuarantineEvent'"
 
 # Merge PDF files
 # Usage: `mergepdf -o output.pdf input{1,2,3}.pdf`
