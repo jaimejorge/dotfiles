d1883285df3b1fea061f6e47921344c18fe0a5de
Cameron Malek <cam@cameronmalek.com>
2015-10-02 13:41:35 -0500

    docker aliases

---
+++ docker/aliases.zsh
@@ -0,0 +1,16 @@
+alias d='docker'
+alias dps='docker ps'
+alias dpsa='docker ps -a'
+alias drm='docker rm -v'
+alias di='docker images'
+
+# Docker Machine
+alias dm='docker-machine'
+alias ddev='eval "$(docker-machine env dev)"'
+
+# Docker Compose
+alias dco='docker-compose'
+alias dcop='docker-compose -p'
+
+# Docker Swarm
+alias ds='docker-swarm'
